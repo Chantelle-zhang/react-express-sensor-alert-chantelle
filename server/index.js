e9d0ace8eea0e40787367c1e255f023c56d85300
chantelle <maudlin.nk@gmail.com>
2019-10-08 10:29:10 +1100

    create server file

--- server/index.js
+++ server/index.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const mockResponse = {
-    foo: 'bar',
-    bar: 'foo'
-};
 
-app.get('/api', (req, res) => {
-    res.send(mockResponse);
-});
-app.get('/', (req, res) => {
-    res.status(200).send('Hello Worl!');
-});
+
+app.use('/api/monitor', require('./routes/api/monitor'));
+
 app.listen(port, function () {
     console.log('App listening on port: ' + port);
 });
