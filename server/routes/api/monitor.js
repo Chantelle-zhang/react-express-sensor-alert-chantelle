404bd8a073b3f70e7fd2ee9dc85332967fbdfafe
chantelle <maudlin.nk@gmail.com>
2019-10-08 10:29:21 +1100

    add api

---
+++ server/routes/api/monitor.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+const BeerData = [
+    {id: '1', name: 'Pilsner', minStoreTemp: '4', maxStoreTemp: '6', currentTemp: null},
+    {id: '2', name: 'IPA', minStoreTemp: '5', maxStoreTemp: '6', currentTemp: null},
+    {id: '3', name: 'Lager', minStoreTemp: '4', maxStoreTemp: '7', currentTemp: null},
+    {id: '4', name: 'Stout', minStoreTemp: '6', maxStoreTemp: '8', currentTemp: null},
+    {id: '5', name: 'Pale Ale', minStoreTemp: '4', maxStoreTemp: '6', currentTemp: null}
+];
+
+router.get('/', async (req, res) => {
+    try {
+        const newBeerData=[...BeerData];
+        await Promise.all([...BeerData].map((beer) => {
+            const url = `https://temperature-sensor-service.herokuapp.com/sensor/${beer.id}`;
+            return axios.get(url)
+        })).then((response) => {
+            const currentTempData=[...response].map((item)=>item.data);
+            newBeerData.map((beer) => {
+                beer.currentTemp = currentTempData.find((item)=>item.id===beer.id).temperature
+            })
+        });
+        res.json(newBeerData)
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
