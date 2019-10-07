a27ca40525942e8cdf8b6f86f0a3d5750f72c0bc
chantelle <maudlin.nk@gmail.com>
2019-10-08 10:27:57 +1100

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,19 @@ module.exports = {
   ],
   rules: {
     "react/jsx-filename-extension": 0,
-    "no-multiple-empty-lines":0
+    "comma-dangle":0,
+    "padded-blocks":0,
+    "react/button-has-type":0,
+    "consistent-return":0,
+    "no-multi-spaces":0,
+    " react/jsx-tag-spacing":0,
+    " react/jsx-wrap-multilines":0,
+    "semi":0,
+    "no-console":0
+
+
+
+
 
   },
 };
