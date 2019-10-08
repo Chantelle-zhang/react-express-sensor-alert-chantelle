8bbf1f7fc32e2a1a3ca61c9e51032247a8c3d511
chantelle <maudlin.nk@gmail.com>
2019-10-09 09:18:18 +1100

    add key property to list,update styling

--- src/component/Alert.js
+++ src/component/Alert.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styles from './Alert.css';
 
 const Alert = () => {
   const unSafeBeers = useSelector((state) => state.unSafeBeers);
@@ -7,8 +8,8 @@ const Alert = () => {
     <div>
       <div className="row">
         {unSafeBeers.length > 0 && unSafeBeers.map((beer) =>
-          <div>
-            <p>
+          <div key={beer.id}>
+            <p className={styles.alert}>
               {`${beer.name} 's temperature is out of safe Range`}
             </p>
           </div>)}
