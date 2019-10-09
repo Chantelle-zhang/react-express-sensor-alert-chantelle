c7483f7198f60004723d5e4ce5fba1728a662896
chantelle <maudlin.nk@gmail.com>
2019-10-09 13:50:14 +1100

    reformat code

--- server/routes/api/monitor/monitor.test.js
+++ server/routes/api/monitor/monitor.test.js
@@ -1,12 +1,12 @@
-const request = require('supertest')
-const app = require('../../../app.js')
+const request = require('supertest');
+const app = require('../../../app.js');
 
 describe('test monitor Endpoints', () => {
   it('should return an array of object ', async () => {
-    const res = await request(app).get('/')
-    const resObject = JSON.parse(res.text)
-    expect(res.statusCode).toEqual(200)
-    expect(resObject[0]).toHaveProperty('isSafe')
+    const res = await request(app).get('/');
+    const resObject = JSON.parse(res.text);
+    expect(res.statusCode).toEqual(200);
+    expect(resObject[0]).toHaveProperty('isSafe');
     expect(resObject).toHaveLength(5)
   },30000)
-})
+});
