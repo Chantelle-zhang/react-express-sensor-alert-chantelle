465d2cc998b599240cdc7712a70028661bcbc12e
chantelle <maudlin.nk@gmail.com>
2019-10-09 16:05:50 +1100

    file structure clean up

--- webpack-config/webpack.common.js
+++ webpack-config/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin}= require('clean-webpack-plugin');
+const {CleanWebpackPlugin}= require('clean-webpack-plugin/dist/clean-webpack-plugin');
 
 module.exports = {
     entry: ['babel-polyfill', './src/index.js'],
